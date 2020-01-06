d8267f357ecfab985629b0d7dc8a331996766516
Josh <og.ginger@gmail.com>
2020-01-06 06:14:05 +0000

    Optimized.

---
+++ build.js
@@ -0,0 +1,12 @@
+//build.js: Configuration file for optimizing the ifSetup function. 
+/*
+*/ 
+({
+    paths: {
+        "validate": "lib/validate.min",
+        "underscore": "lib/underscore.min"
+    },
+    exclude: [ "underscore" ],
+    name: "compose",
+    out: "module_parse.min.js"
+})
